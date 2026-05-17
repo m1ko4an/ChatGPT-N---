63f66f2d07df04da90d6d1a3cf6e276263dc6950
pip <139044696+m1ko4an@users.noreply.github.com>
2026-05-17 23:26:21 +0300

    Create manual.m

---
+++ manual.m
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE book PUBLIC "-//OASIS//DTD DocBook XML V4.5//EN"
+"http://www.oasis-open.org/docbook/xml/4.5/docbookx.dtd">
+
+<book lang="ru">
+
+  <bookinfo>
+    <title>ChatGPT-N</title>
+    <subtitle>Справочное руководство программиста</subtitle>
+
+    <abstract>
+      <para>
+        Документация описывает архитектуру и подсистемы платформы ChatGPT-N.
+      </para>
+    </abstract>
+  </bookinfo>
+
+  <chapter id="overview">
+    <title>Обзор проекта</title>
+
+    <para>
+      ChatGPT-N представляет собой интеллектуальную платформу,
+      объединяющую чат-ботов, генерацию кода,
+      генерацию изображений и обработку документов.
+    </para>
+
+    <para>
+      Архитектура проекта разделена на независимые подсистемы,
+      каждая из которых отвечает за отдельную функциональность.
+    </para>
+  </chapter>
+
+  <chapter id="api-reference">
+    <title>Справочник API</title>
+
+    <section id="ui-module">
+      <title>Подсистема пользовательского интерфейса</title>
+      <para>
+        Обеспечивает взаимодействие пользователя с системой.
+      </para>
+    </section>
+
+    <section id="chatbot-module">
+      <title>Подсистема текстовых чат-ботов</title>
+      <para>
+        Выполняет обработку запросов и генерацию ответов.
+      </para>
+    </section>
+
+    <section id="image-module">
+      <title>Подсистема генерации изображений</title>
+      <para>
+        Создает изображения на основе текстовых запросов.
+      </para>
+    </section>
+
+    <section id="security-module">
+      <title>Подсистема безопасности</title>
+      <para>
+        Обеспечивает защиту данных пользователей.
+      </para>
+    </section>
+
+  </chapter>
+
+</book>
