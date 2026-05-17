dc51bc575e82a6292b474adf65868329b88178fa
pip <139044696+m1ko4an@users.noreply.github.com>
2026-05-17 23:17:55 +0300

    Create security_manager.py

---
+++ security_manager.py
@@ -0,0 +1,23 @@
+"""
+Модуль security_manager.py
+
+Описание:
+    Подсистема безопасности платформы.
+"""
+
+class SecurityManager:
+    """
+    Класс управления безопасностью.
+    """
+
+    def authorize_user(self, login):
+        """
+        Выполняет авторизацию пользователя.
+
+        Args:
+            login (str): Логин пользователя.
+
+        Returns:
+            bool: Результат авторизации.
+        """
+        return True
